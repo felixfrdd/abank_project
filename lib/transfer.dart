1ad63463521589b5b1c9e31733505866bdb2192e
Felix <felixferdinand12@yahoo.com>
2023-11-10 17:05:19 +0700

    Text Transfer

--- lib/transfer.dart
+++ lib/transfer.dart
@@ -9,12 +9,34 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(scaffoldBackgroundColor: Colors.grey),
+        home: const RootPage());
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: SafeArea(
+      child: Column(children: [
+        Center(
+          child: Text(
+            'Transfer',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 25, color: Colors.white),
+          ),
+        )
+      ]),
+    ));
   }
 }
