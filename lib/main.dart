19d4b843063fdf36f4bbb8637f6d4eed6ec7094a
Javier <Javier.gustvin@gmail.com>
2023-11-10 20:07:45 +0700

    Prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,109 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Navbar dengan Gambar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onNavBarItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _selectedIndex == 1 // Only show app bar for the "Transfer" page
+          ? AppBar(
+              backgroundColor: const Color(0xFF363636),
+              title: Image.asset(
+                'assets/ABank.png',
+                height: AppBar().preferredSize.height,
+              ),
+              centerTitle: true,
+            )
+          : null, // Hide app bar for other pages
+      body: Container(
+        color: Colors.grey[200],
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: _getPage(_selectedIndex),
+          ),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onNavBarItemTapped,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black.withOpacity(0.6),
+        selectedLabelStyle: const TextStyle(color: Colors.black),
+        unselectedLabelStyle: TextStyle(color: Colors.black.withOpacity(0.6)),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.swap_horiz),
+            label: 'Transfer',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner),
+            label: 'Scan QR',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
+      ),
     );
+  }
+
+  Widget _getPage(int index) {
+    switch (index) {
+      case 0:
+        return const Center(child: Text('Error'));
+      case 1:
+        return const Text(
+          'Transfer History',
+          style: TextStyle(fontSize: 24),
+        );
+      case 3:
+        return const Center(child: Text('Error'));
+      default:
+        return Container();
+    }
   }
 }
