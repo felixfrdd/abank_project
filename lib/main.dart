f11655662a9e7418595c4dc50f6ca1f1daced87f
Felix <felixferdinand12@yahoo.com>
2023-11-15 00:02:33 +0700

    method to make back button

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
-
 Widget buildBackButton(BuildContext context) {
   return IconButton(
     icon: const Icon(Icons.arrow_back),
