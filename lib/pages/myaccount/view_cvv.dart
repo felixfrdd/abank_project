bf5a58ee3b2e339e77673b3a3c53375d227491ae
Felix <felixferdinand12@yahoo.com>
2023-11-17 16:15:28 +0700

    file structure fixes

---
+++ lib/pages/myaccount/view_cvv.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:abank_project/pages/myaccount/my_account.dart';
+
+class ViewCVV extends StatelessWidget {
+  const ViewCVV({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFF363636),
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("View CVV"),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          leading: BackButton(
+            color: Colors.black,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const MyAccount(),
+                ),
+              );
+            },
+          ),
+        ),
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            margin: const EdgeInsets.only(top: 90),
+            child: Column(
+              children: [
+                Container(
+                  height: 180,
+                  width: 230,
+                  padding: const EdgeInsets.all(10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFD9D9D9),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'icons/cvv.png',
+                            height: 140,
+                            width: 140,
+                            color: Colors.black,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  child: const Text(
+                    'CVV',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  color: const Color(0xFFD9D9D9),
+                  height: 60,
+                  width: 300,
+                  child: const Text(
+                    '167',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
