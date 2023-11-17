4c78eebd5c6ba07d983c90762121b7ca792c63de
Joo <j.torsandy@gmail.com>
2023-11-17 12:31:26 +0700

    scroll

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -15,115 +15,116 @@ class RegistrationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: const Color(0xFF363636),
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              //logo
-              Image.asset(
-                'images/logo.png',
-                width: 360,
-                height: 200,
-              ),
-
-              //welcomeback
-              const Text(
-                'Hi there!',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                //logo
+                Image.asset(
+                  'images/logo.png',
+                  width: 360,
+                  height: 200,
                 ),
-              ),
-              const Text(
-                'Let\'s Get Started',
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 35,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
 
-              MyTextField(
-                controller: fullNameController,
-                hintText: 'Enter your full name',
-                icon: null,
-              ),
-
-              const SizedBox(
-                height: 10,
-              ),
-
-              MyTextField(
-                controller: emailController,
-                hintText: 'Enter your email',
-                icon: null,
-              ),
-
-              const SizedBox(
-                height: 10,
-              ),
-
-              MyTextField(
-                controller: passwordController,
-                hintText: 'Enter password',
-                icon: null,
-              ),
-
-              const SizedBox(
-                height: 10,
-              ),
-
-              MyTextField(
-                controller: confirmPasswordController,
-                hintText: 'Confirm password',
-                icon: null,
-              ),
-
-              const SizedBox(
-                height: 20,
-              ),
-
-              createAcc(),
-
-              const SizedBox(
-                height: 10,
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Already have account? ',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+                //welcomeback
+                const Text(
+                  'Hi there!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                LoginPage()), // Ganti dengan halaman pendaftaran yang sesuai
-                      );
-                    },
-                    child: const Text(
-                      'Login',
+                ),
+                const Text(
+                  'Let\'s Get Started',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 35,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                MyTextField(
+                  controller: fullNameController,
+                  hintText: 'Enter your full name',
+                  icon: null,
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                MyTextField(
+                  controller: emailController,
+                  hintText: 'Enter your email',
+                  icon: null,
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Enter password',
+                  icon: null,
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                MyTextField(
+                  controller: confirmPasswordController,
+                  hintText: 'Confirm password',
+                  icon: null,
+                ),
+
+                const SizedBox(
+                  height: 20,
+                ),
+
+                createAcc(),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Already have an account? ',
                       style: TextStyle(
                         color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                        decoration: TextDecoration.underline,
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
